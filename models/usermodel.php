1796db787db2f65ba48b31512a538b279fa78438
Nauqq <voanhluc258@gmail.com>
2021-10-11 15:43:18 +0700

    Done tam thoi 2

--- models/usermodel.php
+++ models/usermodel.php
@@ -38,47 +38,6 @@
             
             $this->db->closeConnection();
             return $result;
-          /*   if(isset($btnchange)){
-
-                $id = $_GET['id'];
-
-                $fullname = $_POST['fullname'];
-                $username = $_POST['username'];
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-                $repassword = $_POST['repassword'];
-                $birthday = $_POST['birthday'];
-                $website = $_POST['website'];
-                $profileheading = $_POST['profile-heading'];
-                $about = $_POST['about'];
-                $facebook = $_POST['facebook'];
-                $instagram = $_POST['instagram'];
-                $twitter = $_POST['twitter'];
-               
-                if(!empty($fullname) && !empty($username) &&  !empty($email) &&  !empty($password) &&  !empty($repassword) &&  !empty($birthday) &&  !empty($website) &&  !empty($profileheading) &&  !empty($about) &&  !empty($facebook) &&  !empty($instagram)  &&  !empty($twitter)
-                ){
-
-                    if($password == $repassword)  {
-
-                        $result = $this->db->executeNonQuery(" UPDATE `users` set `fullname` = '$fullname', `username` = '$username', `email` = '$email', `password` = '$password', `dob` = '$birthday', `website` = '$website', `heading` = '$profileheading', `about` = '$about',`facebook` = '$facebook', `instagram` = '$instagram', `twitter` = '$twitter'  where `id`  ='$id'");
-
-                        if($result){
-                            echo "<script type='text/javascript'>alert('Cập Nhật Thành Công !');</script>";    
-                        }
-                        else{
-                            echo "<script type='text/javascript'>alert('Cập Nhật Thất Bại !');</script>";    
-                        }
-                    }
-                    else
-                    {
-                        echo "<script type='text/javascript'>alert('Vui lòng nhập repassword giống password !');</script>";
-                    }
-
-                }
-                else{
-                    echo "<script type='text/javascript'>alert('Vui lòng không để trống một trường nào !');</script>";
-                }
-            } */
         }
     }
 ?>
