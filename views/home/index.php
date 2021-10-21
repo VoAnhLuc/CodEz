7e0825da2b01226f4a32a29e8ec4f463ef033edd
HoaiTran308 <tranvanhoai.tt4@gmail.com>
2021-10-21 11:27:41 +0700

    remove lib/boostrap, use direct link from bootstrap

--- views/home/index.php
+++ views/home/index.php
@@ -8,7 +8,8 @@
     <title><?php echo $title ?></title>
     <link rel="stylesheet" href="./lib/css/reset.css">
     <link rel="stylesheet" href="./lib/css/style.css">
-    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
         integrity="sha384-tKLJeE1ALTUwtXlaGjJYM3sejfssWdAaWR2s97axw4xkiAdMzQjtOjgcyw0Y50KU" crossorigin="anonymous">
     <link rel="stylesheet" href="./lib/css/responsive.css">
